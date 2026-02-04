1aa415b1a1588f843a5c252b2a3f51783f925292
Marlon Harrison <92547717+marlon-ionic@users.noreply.github.com>
2026-02-04 16:53:11 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.demo.target.blank',
+  appName: 'Target Blank',
+  webDir: 'www'
+};
+
+export default config;
